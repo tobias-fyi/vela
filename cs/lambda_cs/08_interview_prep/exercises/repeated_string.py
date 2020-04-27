8027bb0ddb9d1895c5a411773e902671a307df1b
tobias-fyi <tobyreaper@gmail.com>
2020-04-27 15:21:46 -0600

    681 lesson - growth mindset and tackling problems

---
+++ cs/lambda_cs/08_interview_prep/exercises/repeated_string.py
@@ -0,0 +1,18 @@
+"""
+Interview prep - day 1 :: Problem solving exercise
+
+HackerRank | Repeated String problem
+https://www.hackerrank.com/challenges/repeated-string/
+"""
+
+def repeatedString(s, n):
+    pass
+
+
+if __name__ == "__main__":
+    s = "aba"
+    n = 10
+
+    result = repeatedString(s, n)
+    print(result)
+
