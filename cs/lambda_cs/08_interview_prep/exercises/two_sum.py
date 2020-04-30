4eadb4cfa43fbd575fcc96e90c0b2215d44dded8
tobias-fyi <tobyreaper@gmail.com>
2020-04-29 21:14:07 -0600

    Transition from CS to DS interview prep

--- cs/lambda_cs/08_interview_prep/exercises/two_sum.py
+++ cs/lambda_cs/08_interview_prep/exercises/two_sum.py
@@ -16,18 +16,51 @@ def two_sum(nums: List[int], target: int) -> List[int]:
     # === Brute force method === #
     # Loop through list
     # Compare value at each index to value at all other indices
-    for i1, val1 in enumerate(nums):
-        print(f"i1: {i1}, val1: {val1}")
-        for i2, val2 in enumerate(nums):
-            print(f"i2: {i2}, val2: {val2}")
-            if i2 == i1:
+    # for i1, val1 in enumerate(nums):
+    #   print(f"i1: {i1}, val1: {val1}")
+    #   for i2, val2 in enumerate(nums):
+    #       print(f"i2: {i2}, val2: {val2}")
+    #       if i2 == i1:
+    #           continue
+    #       if val1 + val2 == target:
+    #           return [i1, i2]
+
+    # === Hash table === #
+    ht = {}
+    # Insert into dict as val: index pairs
+    for index, val in enumerate(nums):
+        ht[val] = index
+
+    # Look for indices that add up to target
+    for index, val in enumerate(nums):
+        diff = target - val
+        match = None
+        # Look up difference in hash table
+        try:
+            match = ht[diff]
+        except KeyError:
+            continue
+
+        if match:
+            if match == index:
                 continue
-            if val1 + val2 == target:
-                return [i1, i2]
+            return [index, match]
 
 
 if __name__ == "__main__":
-    nums = [2, 7, 11, 15]
+    nums = [1, 3, 4, 2,]
 
-    print(two_sum(nums, 9))
+    print(two_sum(nums, 6))
+
+
+
+
+
+
+
+
+
+
+
+
 
