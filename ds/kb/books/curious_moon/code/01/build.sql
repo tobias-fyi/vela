de4223c90008034418eb74c97658b363220a0065
tobias-fyi <tobyreaper@gmail.com>
2020-09-10 18:26:59 -0700

    Started going through A Curious Moon

---
+++ ds/kb/books/curious_moon/code/01/build.sql
@@ -0,0 +1,16 @@
+create schema if not exists import;
+drop table if exists import.master_plan;
+create table import.master_plan(
+    start_time_utc text,
+    duration text,
+    date text,
+    team text,
+    spass_type text,
+    target text,
+    request_name text,
+    library_definition text,
+    title text,
+    description text
+);
+COPY import.master_plan
+FROM '/Users/Tobias/workshop/vela/ds/kb/books/curious_moon/data/master_plan.csv' WITH DELIMITER ',' HEADER CSV;
---
+++ ds/kb/books/curious_moon/code/02/scripts/import.sql
@@ -0,0 +1,13 @@
+drop table if exists import.master_plan;
+create table import.master_plan(
+    start_time_utc text,
+    duration text,
+    date text,
+    team text,
+    spass_type text,
+    target text,
+    request_name text,
+    library_definition text,
+    title text,
+    description text
+);
